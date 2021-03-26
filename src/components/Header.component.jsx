4fce1914e0c621094b2524d4b681962013af0684
Aadit <baggaaadit017@gmail.com>
2021-03-26 11:54:10 +0530

    added styled components

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import styled from "styled-components"
+
 
 function Header() {
     return (
         <div>
-            THis is the header component
+            <HeaderLogo />
         </div>
     )
 }
 
 export default Header;
+const HeaderLogo=styled.div`
+    height:60px;
+    background-color:#0F1111
+`
+ 
