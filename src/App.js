6a5ec520e0ed995029f652f7cdf67b36f4a80621
Aadit <baggaaadit017@gmail.com>
2021-03-26 13:11:08 +0530

    wokring on header

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Header from "./components/Header.component"
 
 function App() {
   return (
-    <div className="App">
-    <Header />
+    <div>
+      <Header />
     </div>
   );
 }
--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -1,18 +1,72 @@
 import React from 'react'
 import styled from "styled-components"
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+const Header=()=>{
+    return (   
+        <Container>
+            <HeaderLogo>
+            <img src={"https://i.imgur.com/7I9Was5.png"} alt={"dc"} />
+            </HeaderLogo>
+                <HeaderAdressOption> 
+                    <OptionLineOne>Hello</OptionLineOne>
+                    <OptionLineTwo>Select your address</OptionLineTwo>
+                </HeaderAdressOption>
 
+                <HeaderSearch>
+                    <HeaderSearchInput type="text" />
+                    <HeaderSearchIconContainer>
+                        <SearchIcon />
+                    </HeaderSearchIconContainer>
+                </HeaderSearch>
 
-function Header() {
-    return (
-        <div>
-            <HeaderLogo />
-        </div>
-    )
+                <HeaderNavItems>
+                    <HeaderOption>
+                    <OptionLineOne>Hello,Aadit</OptionLineOne>
+                    <OptionLineTwo>Accounts & Lists</OptionLineTwo>
+                    </HeaderOption>
+                    <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>& Orders</OptionLineTwo>
+                    </HeaderOption>
+                    <HeaderOptionCart>
+
+                    </HeaderOptionCart>
+                </HeaderNavItems>
+            </Container>
+        )
 }
 
 export default Header;
-const HeaderLogo=styled.div`
+const Container=styled.div`
     height:60px;
-    background-color:#0F1111
+    background-color:#0F1111;
+    display:flex;
+    align-items:center;
+    color:white;
 `
- 
+
+const HeaderLogo=styled.div`
+    img{ 
+        width:100px;
+        margin-left:11px;
+    }
+ `
+const HeaderAdressOption=styled.div`
+
+`
+const OptionLineOne=styled.div`
+
+`
+const OptionLineTwo=styled.div`
+
+`
+const HeaderSearch=styled.div`
+display:flex;
+`
+const HeaderSearchInput=styled.input`
+
+`
+const HeaderSearchIconContainer=styled.div`
+
+`
